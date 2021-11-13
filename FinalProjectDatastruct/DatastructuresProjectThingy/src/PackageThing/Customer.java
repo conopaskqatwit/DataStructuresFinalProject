9bdf362de35081c170d3fd351fbc5d229952d40c
conopaskqatwit <71512951+conopaskqatwit@users.noreply.github.com>
2021-11-13 11:46:38 -0500

    Update Customer.java
    finished not tested

--- FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Customer.java
+++ FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Customer.java
@@ -1,50 +1,68 @@
 package PackageThing;
 
+import java.sql.Array;
+
 public class Customer {
 	private double totalPrice;
 	private String Name;
 	private ArrayBag<MenuItems> Cart;
-	public Customer() {
-		
-	}
-	public Customer(String name) {
-		
-	}
-/**return true if able to select item otherwise false
- * 
- * @param item
- * @return
- */
-	public boolean selectItem(MenuItems item) {
 
-		return false;
+	public Customer(String name) {
+		this.Name = name;
 	}
+
+	/**
+	 * return true if able to select item otherwise false
+	 * 
+	 * @param item
+	 * @return
+	 */
+	public boolean selectItem(MenuItems item) {
+		Cart.add(item);
+		return true;
+	}
+
 	/**
 	 * 
 	 * 
 	 * Displays totalPrice and sends items in cart to kitchen/barista queue
+	 * 
 	 * @return boolean
-	*/
-	public boolean checkOut() {
-	
-	return false;
+	 */
+	public boolean checkOut(Kitchen k) {
+		MenuItems[] cartArray = (MenuItems[]) new Array[Cart.getCurrentSize()];
+		cartArray = Cart.toArray();
+		int cartsize = cartArray.length;
+		double totalprice = 0;
+		for (int i = 0; i < cartsize; i++) {
+			totalprice = totalprice + cartArray[i].price;
+		}
+		System.out.println("Your subtotal is: " + totalprice);
+		System.out.printf("MA tax is: %.2f%n", totalprice * 0.625);
+		System.out.printf("Your total is: %.2f%n", (totalprice * 0.625 + totalprice));
+		this.totalPrice = totalprice * 0.625 + totalprice;
+		k.orders.enqueue(Cart);
+		return true;
 	}
-	/**Removes item from cart
+
+	/**
+	 * Removes item from cart
+	 * 
 	 * @param item
 	 * @return
 	 */
 	public MenuItems removeItem(MenuItems item) {
-	
-	return item;
+		Cart.remove(item);
+		return item;
 	}
+
 	/**
 	 * sets name of the bag
+	 * 
 	 * @param name
 	 */
-	public void setName(String name){
+	public void setName(String name) {
+		this.Name = name;
 	}
-	
-	
-	
 
 }

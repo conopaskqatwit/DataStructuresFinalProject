34c662803d45692c5530750e45d27ff1b4cf4171
conopaskqatwit <71512951+conopaskqatwit@users.noreply.github.com>
2021-11-13 14:58:02 -0500

    Update Menu.java

--- FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Menu.java
+++ FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Menu.java
@@ -1,33 +1,33 @@
 package PackageThing;
 
-public class Menu {
-	private ArrayBag<String> drinks;
-	private ArrayBag<String> food;
+public final class Menu {
+	public static ArrayBag<String> drinks;
+	public static ArrayBag<String> food;
 	public Menu() {
 		
 	}
 	/**
 	 * prints drink bag to console
 	 */
-	public void displayDrinks() {
+	public static void displayDrinks() {
 		
 	}
 	/**
 	 * prints food bag to console
 	 */
-	public void displayFood() {
+	public static void displayFood() {
 		
 	}
 	/**
 	 * sorts bag items by price
 	 */
-	public void sortPrice() {
+	public static void sortPrice() {
 		
 	}
 	/**
 	 * sorts bag items by name alphabetically
 	 */
-	public void sortName() {
+	public static void sortName() {
 		
 	}
 

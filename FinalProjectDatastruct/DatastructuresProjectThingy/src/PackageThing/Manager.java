eea9f08e4516265e09dc18637d9430f1d91ba0c3
mikkelsonmatwit <71507351+mikkelsonmatwit@users.noreply.github.com>
2021-11-13 11:44:17 -0500

    Update Manager.java

--- FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Manager.java
+++ FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Manager.java
@@ -1,30 +1,66 @@
 package PackageThing;
 
+import java.util.Scanner;
+
 public class Manager {
-private int DEFAULT_PIN = 0000;
-public int pin;
+	private int DEFAULT_PIN = 0000;
+	public int pin;
 
-/**
- * changes manager Pin
- * @param newPin
- */
-public void changePin(int newPin) {
-	
-}
-/**
- * adds item to menu
- * @param newitem
- */
- public void addItem(MenuItems newitem) {
+	/**
+	 * changes manager Pin
+	 */
+	public void changePin(Scanner input) {
+		System.out.println("Enter Pin:");
+		int oldPin = input.nextInt();
+		if(oldPin == this.pin) {
+			System.out.println("Enter new Pin:");
+			this.pin = input.nextInt();
+		} else {
+			System.out.println("Pin is incorrect");
+			return;
+		}
+	}
+	/**
+	 * adds item to menu
+	 * @param newitem
+	 */
+	 public void addItem(Scanner input, Menu m) {
+		 System.out.println("Name of Item: ");
+		 String name = input.nextLine();
+		 
+		 System.out.println("Item Description: ");
+		 String description = input.nextLine();
+		 
+		 System.out.println("Price: ");
+		 double price = input.nextInt();
+		 
+		 System.out.println("Type (1 if drink, 2 if food):");
+		 int type = input.nextInt();
+		 
+		 if(type == 1) {
+			 m.drinks.add(new MenuItems(price, name, description, type));
+		 } else if(type == 2) {
+			 m.food.add(new MenuItems(price, name, description, type));
+		 }
+		 
+		 return;
+		 
+	 }
 	 
- }
-
- /**
-  * removes item from menu
-  * @param olditem
-  */
-public void removeItem(MenuItems olditem) {
 	
-
-}
+	 /**
+	 * removes item from menu
+	 * @param olditem
+	 */
+	public void removeItem(MenuItems oldItem, Menu m) {
+		
+		if(oldItem.getType() == 1) {
+			m.drinks.remove(m.drinks.getIndex(oldItem));
+		} else if(oldItem.getType() == 2) {
+			m.food.remove(m.food.getIndex(oldItem));
+		}
+		
+		return;
+	
+	}
 }

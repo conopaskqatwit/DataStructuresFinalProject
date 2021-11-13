52d82d42cf4fe7d8f73ff19895164c42038deb14
mikkelsonmatwit <71507351+mikkelsonmatwit@users.noreply.github.com>
2021-11-13 11:29:38 -0500

    Update Kitchen.java

--- FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Kitchen.java
+++ FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Kitchen.java
@@ -1,7 +1,7 @@
 package PackageThing;
 
 public class Kitchen {
-	public CircularLinkedQueue orders;
+	public CircularLinkedQueue<ArrayBag<MenuItems>> orders;
 	
 /**
  * marks the food part of order as complete

29b9b3ef02b2e8e0f59d1b8cea2392cbac5d69d5
Michael Nguyen <nguyenm33@wit.edu>
2021-11-21 21:59:47 -0500

    Update MenuItems.java

--- FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/MenuItems.java
+++ FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/MenuItems.java
@@ -7,82 +7,100 @@ public class MenuItems {
 	public String name;
 	private String description;
 	private int type;
+
 	public MenuItems() {
-		
+
 	}
+
 	public MenuItems(double price, String name, String description, int type) {
 		this.price = price;
 		this.name = name;
 		this.description = description;
 		this.type = type;
 	}
-/**
- * change price of an item
- * @param newPrice
- */
+
+	/**
+	 * change price of an item
+	 * 
+	 * @param newPrice
+	 */
 	public void changePrice(double newPrice) {
-	this.price = newPrice;
+		this.price = newPrice;
 	}
-/**
- * change Description
- * @param newDescription
- */
+
+	/**
+	 * change Description
+	 * 
+	 * @param newDescription
+	 */
 	public void changeDescription(String newDescription) {
-	this.description = newDescription;
+		this.description = newDescription;
 	}
-/**
- * changes type
- * @param newType
- */
+
+	/**
+	 * changes type
+	 * 
+	 * @param newType
+	 */
 	public void changeType(int newType) {
-	this.type = newType;
+		this.type = newType;
 	}
-/**
- * changes name
- * @param newName
- */
+
+	/**
+	 * changes name
+	 * 
+	 * @param newName
+	 */
 	public void changeName(String newName) {
 		this.name = newName;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public int getType() {
 		return this.type;
 	}
-	
+
 	public boolean equals() {
 		return false;
 	}
-	public double getPrice(){
+
+	public double getPrice() {
 		return this.price;
 	}
+
 	// to string method
 	/**
 	 * display all the items in the menu
+	 * 
 	 * @return
 	 */
 	public String displayMenuItems() {
-		String items = "Price: " + price +"Name of item: " + name +"Description: " + "Type: " + type;
+		String items = "Price: " + price + "Name of item: " + name + "Description: " + "Type: " + type;
 		return items;
 	}
-	
-	public static Comparator<MenuItems> sortByPrice=new Comparator<MenuItems>() {
-		@Override
-		public int compare(MenuItems a, MenuItems b) {
-			return a.getPrice() < b.getPrice() ? -1 : a.getPrice() == b.getPrice() ? 0 : 1;
-		}
-	};
-	
-	public static Comparator<MenuItems> sortByName=new Comparator<MenuItems>() {
-		@Override
-		public int compare(MenuItems a, MenuItems b) {
-			return a.name.compareToIgnoreCase(b.name);
-		}
-	};
-	
 }
 
+class PriceComparator implements Comparator<MenuItems> {
+	@Override
+	/*compares two items of type MenuItems and 
+	 * returns a negative integer, zero, or a positive 
+	 * integer as the first argument is less than, equal to, or greater than the second.
+	 */
+	public int compare(MenuItems a, MenuItems b) {
+		return a.getPrice() < b.getPrice() ? -1 : a.getPrice() == b.getPrice() ? 0 : 1;
+	}
+}
 
+class NameComparator implements Comparator<MenuItems> {
+	@Override
+	/*compares two items of type MenuItems and 
+	 * returns a negative integer, zero, or a positive 
+	 * integer as the first argument is less than, equal to, or greater than the second.
+	 */
+	public int compare(MenuItems a, MenuItems b) {
+		return a.name.compareToIgnoreCase(b.name);
+	}
+};

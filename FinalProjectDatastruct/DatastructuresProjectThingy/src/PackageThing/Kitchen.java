612ac6741c5530592dca68176ebd240df188139c
mikkelsonmatwit <71507351+mikkelsonmatwit@users.noreply.github.com>
2021-11-27 01:49:49 -0500

    Update Kitchen.java

--- FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Kitchen.java
+++ FinalProjectDatastruct/DatastructuresProjectThingy/src/PackageThing/Kitchen.java
@@ -1,20 +1,46 @@
 package PackageThing;
 
 public class Kitchen {
-	public CircularLinkedQueue<ArrayBag<MenuItems>> orders;
-	
-/**
- * marks the food part of order as complete
- * @param order
- * @return
- */
-	public boolean markDone(ArrayBag order) {
-	if (order.isEmpty())
-		return true;
-		
-		
-	else {
-		return false;
+	public static CircularLinkedQueue<ArrayBag<MenuItems>> orders = new CircularLinkedQueue<ArrayBag<MenuItems>>();
+
+	/**
+	 * marks the food part of order as complete
+	 * 
+	 * @param order
+	 * @return
+	 */
+	public static void markNextOrderDone() {
+		try {
+			ArrayBag<MenuItems> filledOrder = orders.dequeue();
+		} catch (EmptyQueueException e) {
+			System.out.println("There are no orders to be filled.  Logging out... ");
+			return;
+		}
 	}
-   }
+	
+	public static void displayNextOrder() {
+		try {
+			ArrayBag<MenuItems> nextOrder = orders.getFront();
+			if (nextOrder.isEmpty()) {
+				orders.dequeue();
+				displayNextOrder();
+			} else {
+				System.out.println("Next Order: ");
+				MenuItems item;
+				ArrayBag<MenuItems> temp = new ArrayBag<>();
+				while(!nextOrder.isEmpty()) {
+					temp.add(nextOrder.remove());
+				}
+		        while(!temp.isEmpty()) {
+		        	item = temp.remove();
+		        	System.out.println(item.getName());
+		        	nextOrder.add(item);
+		        }
+			}
+		} catch (EmptyQueueException e) {
+			System.out.println("There are no orders to be filled.  Logging out... ");
+			return;
+		}
+        return;
+	}
 }
